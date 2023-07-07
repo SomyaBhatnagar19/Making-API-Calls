f80c1e7a45f6ea4559c75408492b0f95c9460046
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-07-07 12:59:39 +0530

    Adding isLoading feature to the site along with no data found when button not pressed.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [ movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  //for loading movies
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovieHandler = async () => {
-    try{
-      const response = await fetch('https://swapi.dev/api/films/');
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films/");
       const data = await response.json();
-    
-      const transformedMovies = data.results.map(movieData => {
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_data
-        }
-      })
+          releaseDate: movieData.release_data,
+        };
+      });
       setMovies(transformedMovies);
-    }
-    catch (error) {
+      setIsLoading(false);
+    } catch (error) {
       console.log(error);
     }
-    
-  }
-  
+  };
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Movies not found.</p>}
+        {isLoading && (
+          <div className="parent-container">
+            <div className="spinner-container">
+            <div className="loading-spinner"></div>
+          </div>
+          </div>
+          
+        )}
       </section>
     </React.Fragment>
   );
